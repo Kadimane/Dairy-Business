0257cbedde2aaab0e05000438af1c65007366f35
Kadimane <kadimanegloriah@gmail.com>
2021-05-03 10:23:24 +0300

    adds

---
+++ main.js
@@ -0,0 +1,8 @@
+var price=45;
+var yearProduction=[
+{"period":"January","production":1876,"days":31},
+{"period":"Febuary","production":1876,"days":28},
+{"period":"March","production":1876,"days":31},
+{"period":"April","production":1876,"days":30},
+{"period":"May","production":1876,"days":31},
+{"period":"June","production":1876,"days":30},
